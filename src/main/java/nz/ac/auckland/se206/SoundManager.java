c936e738daa3384e8e21f76a2529dcd9ce586952
HamishPatel <hpat874@aucklanduni.ac.nz>
2023-09-17 18:21:26 +1200

    This adds the new select sound for the start main menu screen

--- src/main/java/nz/ac/auckland/se206/SoundManager.java
+++ src/main/java/nz/ac/auckland/se206/SoundManager.java
@@ -9,6 +9,8 @@ public class SoundManager {
   private static MediaPlayer mediaPlayer;
   private static Media soundClick =
       new Media(App.class.getResource("/sounds/click.mp3").toString());
+  private static Media soundSelect =
+      new Media(App.class.getResource("/sounds/select.mp3").toString());
 
   /** Plays the click sound */
   public static void playClick() {
@@ -16,6 +18,15 @@ public class SoundManager {
       mediaPlayer.stop();
     }
     mediaPlayer = new MediaPlayer(soundClick);
+    mediaPlayer.play();
+  }
+
+  /** Plays the select sound */
+  public static void playSelect() {
+    if (mediaPlayer != null) {
+      mediaPlayer.stop();
+    }
+    mediaPlayer = new MediaPlayer(soundSelect);
     mediaPlayer.play();
   }
 }
